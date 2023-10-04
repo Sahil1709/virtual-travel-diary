cdd59eed2d92bc03e14757ac0fe594f3ada5cd23
Sahil Gupta <sahilofficial17@gmail.com>
2023-10-03 20:08:40 -0700

    FIrebase Setup

---
+++ app/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+//import firebase from 'firebase/app'
+import 'firebase/storage'
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_API_ID,
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage(app);
+export const database = getFirestore(app);
+export const auth = getAuth(app);
